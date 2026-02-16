1245ad5c24dcda32de164fb794afcd6c70e4bea8
Dmitry Barashev <dbarashev@ganttproject.biz>
2026-02-17 01:15:47 +0400

    GanttProject 3.4 Beta III

--- ganttproject/src/main/java/biz/ganttproject/app/Splash.kt
+++ ganttproject/src/main/java/biz/ganttproject/app/Splash.kt
@@ -53,7 +53,7 @@ fun showAsync(): CompletableFuture<Runnable> {
       it.styleClass.add("heart")
     }
 
-    val textSplash = Label("3.4 Beta II").also {
+    val textSplash = Label("3.4 Beta III").also {
       it.styleClass.add("version")
     }
     val stackPane = StackPane().also {
